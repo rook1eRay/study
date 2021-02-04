6012ec95d04422b9efe485524a008b4f62a950bb
rook1eRay <aqhnpzr@163.com>
2021-02-04 19:35:32 +0800

    netty-demo first commit

---
+++ netty-demo/src/main/java/com/huaizhong/demo/netty/NettyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.huaizhong.demo.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyDemoApplication.class, args);
+    }
+
+}
---
+++ netty-demo/src/test/java/com/huaizhong/demo/netty/NettyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huaizhong.demo.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
